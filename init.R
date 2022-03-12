34ebf25966d31ed753cc53151974860e6b90b3b3
Katia Aristova <k.aristova@gmail.com>
2022-03-12 02:02:53 -0800

    Add heroku files

---
+++ init.R
@@ -0,0 +1,11 @@
+r <- getOption('repos')
+r['CRAN'] <- 'http://cloud.r-project.org'
+options(repos=r)
+
+# ======================================================================
+
+
+
+# packages go here
+install.packages(c('dash', 'dplyr', 'ggplot2', 'plotly', 'purrr', 'stringr'))
+remotes::install_github('facultyai/dash-bootstrap-components@r-release')
