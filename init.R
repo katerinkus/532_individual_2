7e940ae41305472c84e54ebec2cce037d1139a28
Katia Aristova <k.aristova@gmail.com>
2022-03-12 03:23:44 -0800

    Add version

--- init.R
+++ init.R
@@ -7,5 +7,6 @@ options(repos=r)
 
 
 # packages go here
-install.packages(c('remotes', 'dash', 'dplyr', 'ggplot2', 'plotly', 'purrr', 'stringr'))
+install.packages(c('remotes', 'dash', 'dplyr', 'ggplot2', 'plotly', 'stringr'))
+install.versions(c('purrr'), c('0.3.4'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
